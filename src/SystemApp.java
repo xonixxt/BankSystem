7387ac79d3c2d157c4d4489972981e994339ba28
xoni.xxt <jgabrielwrk@gmail.com>
2025-07-27 21:35:34 -0300

    Main outline

--- src/SystemApp.java
+++ src/SystemApp.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class SystemApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bank bank = new Bank();
 
+        while (true) {
+            System.out.println("\n=== Bank System ===");
+            System.out.println("1. Create account");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Transfer");
+            System.out.println("5. View account");
+            System.out.println("6. List all accounts");
+            System.out.println("0. Exit");
+            System.out.print("Choose an option: ");
+
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            break;
+        }
+
+        sc.close();
     }
 }
